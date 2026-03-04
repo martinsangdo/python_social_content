343203254c1a57ef818336470cf925b58916939c
Martin Sang Do <martin.do@zinc.today>
2026-03-04 21:50:38 +0700

    - merge random video & img for Math for Kids

--- core/utilities.py
+++ core/utilities.py
@@ -1,8 +1,14 @@
 from pathlib import Path
-
+import random
 
 def list_video_in_path(folder_path):
     return list(Path(folder_path).glob("*.mp4"))
 
 def list_audio_in_path(folder_path):
     return list(Path(folder_path).glob("*.mp3"))
+
+def list_image_in_path(folder_path):
+    return list(Path(folder_path).glob("*.png"))
+
+def get_random_item(items):
+    return random.choice(items)
